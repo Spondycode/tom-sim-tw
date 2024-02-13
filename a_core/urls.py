9e384746e2219e98ccfa28359aae58cff6e3d429
David Allen <spondicious@me.com>
2024-02-13 10:11:02 +0100

    first commit

---
+++ a_core/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for a_core project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("sim.urls")),
+]
